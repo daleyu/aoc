0d34fe574a99d901ea2558f0563ed8a90f44ca6f
daleyu <yudale2003@gmail.com>
2025-02-02 14:02:16 -0800

    add utils and go mod init

---
+++ 2015/1/q1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/daleyu/aoc-practice/utils/stack"
+)
+
+func main() {
+	result := 0
+
+	//Use a stack and maintain polarity on if it is negative or not
+	polarity := 1
+	s := stack.New()
+
+	fmt.Printf("Floor: %x", result)
+
+}
