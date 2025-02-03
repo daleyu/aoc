20993e9dbdef87844ecdaab29bf35aefa0e55c6d
dale.yu <yudale2003@gmail.com>
2025-02-03 14:33:40 -0800

    git signature is fixed to personal now

--- 2015/2/q1.go
+++ 2015/2/q1.go
@@ -20,20 +20,18 @@ func main() {
 	check(err)
 	for _, line := range strings.Split(string(input), "\n") {
 		dims := strings.Split(line, "x")
-		extraSlack, e := strconv.Atoi(dims[0])
-		check(e)
-		for _, side := range dims {
-			if side <= extraSlack {
-				extraSlack = side
-			}
-		}
 		l, e := strconv.Atoi(dims[0])
 		check(e)
 		w, e := strconv.Atoi(dims[1])
 		check(e)
 		h, e := strconv.Atoi(dims[2])
 		check(e)
-		fmt.Printf("%d * %d * %d = %d\n", l, w, h, result)
+		extraSlack := l * w
+		if extraSlack > l*h {
+			extraSlack = l * h
+		} else if extraSlack > w*h {
+			extraSlack = w * h
+		}
 		result += (2 * l * w) + (2 * w * h) + (2 * l * h) + extraSlack
 	}
 
