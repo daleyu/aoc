a9b074fd66003943ed58fe7f9961ad930cd435fd
daleyu <yudale2003@gmail.com>
2025-02-05 17:46:22 -0800

    day 6 part1

--- 2015/6/main.go
+++ 2015/6/main.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"os"
 	"strings"
-
-	"github.com/daleyu/aoc-practice/data-structures/set"
 )
 
 func check(e error) {
@@ -27,75 +25,71 @@ func main() {
 	check(err)
 	resultPart1 := part1(string(input))
 
-	fmt.Printf("Nice Strings: %d\n", resultPart1)
+	fmt.Printf("Lights on: %d\n", resultPart1)
 
-	resultPart2 := part2(string(input))
-	fmt.Printf("Nice Strings Part 2: %d\n", resultPart2)
+	// resultPart2 := part2(string(input))
+	// fmt.Printf("Light", resultPart2)
 }
-
-var vowels = set.NewStringSet("a", "e", "i", "o", "u")
-var forbidden = set.NewStringSet("ab", "cd", "pq", "xy")
 
 func part1(input string) int {
 	result := 0
-	for _, line := range strings.Split(input, "\n") {
-		vowelCount := 0
-		repeatCount := 0
-		failed := false
-		for i, c := range line {
-			cha := string(c)
-			if vowels.Has(cha) {
-				vowelCount++
-			}
-			if i < len(line)-1 {
-				subseq := line[i : i+2]
-				if forbidden.Has(subseq) {
-					failed = true
-				}
-
-				if line[i] == line[i+1] {
-					repeatCount++
-				}
-			}
-		}
-		if failed {
-			continue
-		}
-
-		if vowelCount >= 3 && repeatCount >= 1 {
-			result++
-		}
+	grid := make([][]bool, 1000)
+	for i := range grid {
+		grid[i] = make([]bool, 1000)
 	}
 
+	for _, line := range strings.Split(input, "\n") {
+		toggle := false
+		on := false
+		startX := 0
+		startY := 0
+		endX := 0
+		endY := 0
+		line := string(line)
+
+		switch {
+		case strings.HasPrefix(line, "toggle"):
+			fmt.Sscanf(line, "toggle %d,%d through %d,%d", &startX, &startY, &endX, &endY)
+			toggle = true
+		case strings.HasPrefix(line, "turn on"):
+			fmt.Sscanf(line, "turn off %d,%d through %d,%d", &startX, &startY, &endX, &endY)
+			on = true
+		case strings.HasPrefix(line, "turn off"):
+			fmt.Sscanf(line, "turn on %d,%d through %d,%d", &startX, &startY, &endX, &endY)
+			on = false
+		default:
+			fmt.Println(line)
+			fmt.Println("Something must be wrong with input")
+		}
+
+		for i := startX; i <= endX; i++ {
+			for j := startY; j <= endY; j++ {
+				if toggle {
+					grid[i][j] = !grid[i][j]
+					continue
+				}
+				if on {
+					grid[i][j] = true
+				} else {
+					grid[i][j] = false
+				}
+			}
+		}
+
+	}
+
+	for i := 0; i < 1000; i++ {
+		for j := 0; j < 1000; j++ {
+			if grid[i][j] {
+				fmt.Printf("x: %d, y: %d\n", i, j)
+				result++
+			}
+		}
+	}
 	return result
 }
 
 func part2(input string) int {
-	checkPair := func(line string) bool {
-		for i := 0; i < len(line)-2; i++ {
-			temp := line[i : i+2]
-			for j := i + 2; j < len(line)-1; j++ {
-				if temp == line[j:j+2] {
-					return true
-				}
-			}
-
-		}
-		return false
-	}
 	result := 0
-	for _, line := range strings.Split(input, "\n") {
-		pairsPossible := checkPair(line)
-		if !pairsPossible {
-			continue
-		}
-		for i := 0; i < len(line)-2; i++ {
-			if line[i] == line[i+2] {
-				result++
-				break
-			}
-		}
-	}
-
 	return result
 }
