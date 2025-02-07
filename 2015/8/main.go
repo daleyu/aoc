6e8a3e64cea63e77d8d2736ffc26025784af270a
dale.yu <yudale2003@gmail.com>
2025-02-07 15:04:46 -0800

    finish day 8

--- 2015/8/main.go
+++ 2015/8/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func check(e error) {
@@ -24,14 +26,41 @@ func main() {
 	check(err)
 	resultPart1 := part1(string(input))
 
-	fmt.Printf("Value on a: %d\n", resultPart1)
+	fmt.Printf("Part 1: %d\n", resultPart1)
 
-	// resultPart2 := part2(string(input))
-	// fmt.Printf("New value: %d\n", resultPart2)
+	resultPart2 := part2(string(input))
+	fmt.Printf("Part 2: %d\n", resultPart2)
 }
 
+func unquote(text string) string {
+	s, err := strconv.Unquote(text)
+	check(err)
+
+	return s
+}
+
+func quote(text string) string {
+	s := strconv.Quote(text)
+	return s
+}
+
+// Disregarding the whitespace in the file, what is the number of characters of
+// code for string literals minus the number of characters in memory for the
+// values of the strings in total for the entire file?
 func part1(input string) int {
 	var result int
+	for _, line := range strings.Split(input, "\n") {
+		result += len(line) - len(unquote(line))
+	}
+
+	return result
+}
+
+func part2(input string) int {
+	var result int
+	for _, line := range strings.Split(input, "\n") {
+		result += len(quote(line)) - len(line)
+	}
 
 	return result
 }
