d6ceefddffc942912247b86850fe9c3955078696
daleyu <yudale2003@gmail.com>
2025-02-12 23:21:00 -0800

    finish 2016 day 1

--- 2016/1/main.go
+++ 2016/1/main.go
@@ -75,9 +75,30 @@ func part1(input string) int{
 }
 
 func part2(input string) int{
-	result := 0 
+	visited := make(map[[2]int]bool)
+	visited[[2]int{0,0}] = true
+	x := 0
+	y := 0 
+	for _, move := range strings.Split(input, ","){
+		move = strings.TrimSpace(move)
+		turn := string(move[0])
+		magnitude, e := strconv.Atoi(string(move[1:]))
+		check(e)
+		fmt.Println(turn)
+		changeDirection(turn)
 
-	return result
+		for i := 0; i < magnitude; i++ {
+			x += directions[index][0] 
+			y += directions[index][1]
+			curr := [2]int{x,y}
+			if visited[curr] {
+				return int(math.Abs(float64(x))) + int(math.Abs(float64(y)))
+			}
+			visited[curr] = true
+		}
+	}
+	fmt.Println("We got to the end without ever getting to the easter bunny")
+	return int(math.Abs(float64(x))) + int(math.Abs(float64(y)))
 }
 
 
