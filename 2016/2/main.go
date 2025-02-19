45d7d099e914ab1c43545ef61286faa9d6d98b54
dale.yu <yudale2003@gmail.com>
2025-02-19 09:06:42 -0800

    part1

--- 2016/2/main.go
+++ 2016/2/main.go
@@ -35,30 +35,50 @@ func appendToNum(source, num int) int {
 }
 
 func calcNum(x, y int) int{
-	return x * 3 + y
+	return (x * 3) + (y + 1)
 }
 
-func moveButton(
-
+func moveButton(x, y *int, dx, dy int){
+	if *x + dx < 0 {
+		*x = 0
+	} else if *x + dx > 2 {
+		*x = 2
+	} else {
+		*x += dx
+	}
+	
+	if *y + dy < 0 {
+		*y = 0
+	} else if *y + dy > 2 {
+		*y = 2
+	} else {
+		*y += dy
+	}
+}
 func part1(input string) int{
 	result := 0 
 	var mapping = map[string][]int{
-		"U": {1,0},
-		"D": {0,1},
-		"L": {-1,0},
-		"R": {0,-1},
+		"U": {-1,0},
+		"D": {1,0},
+		"L": {0,-1},
+		"R": {0,1},
 	}
 	x := 1
 	y := 1
 	for _, line := range strings.Split(input, "\n") {
-		for _, cha := range strings.Split(line, ""){
-			change := mapping[cha]
-			dx, dy := change[0], change[1]
-			x += dx 
-			y += dy 
-			num := calcNum(x,y)
-			result = appendToNum(result, num)
+		print(line)
+		if len(line) == 0{
+			break
 		}
+		for _, cha := range line {
+			change := mapping[string(cha)]
+			dx, dy := change[0], change[1]
+			moveButton(&x,&y, dx, dy)
+		}
+		fmt.Printf("x: %d, y: %d \n", x,y)
+		num := calcNum(x,y)
+		fmt.Println(num)
+		result = appendToNum(result, num)
 	}
 
 	return result
